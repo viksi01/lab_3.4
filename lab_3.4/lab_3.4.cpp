03b5e26966063ff0bb7353a5aacdcdb78f67c28b
viksi01 <victoria.pyatnicka@gmail.com>
2022-10-14 02:11:02 +0300

    commit1_master

---
+++ lab_3.4/lab_3.4.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double R;
+    double x;
+    double y;
+
+    cout << "R = "; cin >> R;
+    cout << "x = "; cin >> x;
+    cout << "y = "; cin >> y;
+
+    if ((y >= -x - 2 * R && y <= 0 && x <= 0 && x >= -2 * R) || (y >= sqrt(R * R - x * x) && y <= 2 * R && x >= 0 && x <= 2 * R))
+        cout << "yes" << endl;
+    else
+        cout << "no" << endl;
+
+    return 0;
+}
